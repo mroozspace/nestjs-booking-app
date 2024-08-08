74389cc2dd104e9f7e2bfc637b5aabd8cdd6de4c
Adrian Mróz <adrianmroz.dev@gmail.com>
2024-08-08 12:38:54 +0200

    add validation and logging

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date) // transform to date
   startDate: Date;
+
+  @IsDate()
+  @Type(() => Date) // transform to date
   endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
