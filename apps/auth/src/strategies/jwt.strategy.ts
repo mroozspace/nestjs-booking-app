f6a8e92e412581df8b995be5778bb92572bedcce
Adrian Mróz <adrianmroz.dev@gmail.com>
2024-08-12 12:21:39 +0200

    add jwt strategy for authentication

---
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../users/users.service';
+import { Request } from 'express';
+import { TokenPayload } from 'apps/auth/interfaces/token-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private readonly userService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.Authentication;
+        },
+      ]),
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate({ userId }: TokenPayload) {
+    return this.userService.getUser({ _id: userId });
+  }
+}
--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { CurrentUser } from '../current-user.decorator';
+import { UserDocument } from './model/user.schema';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard copy';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  @Get()
+  @Get('all') // for debugging
   async getUsers() {
     return this.usersService.find();
   }
@@ -13,5 +16,12 @@ export class UsersController {
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  // necessary to install cookie-parser if still gets 401
+  async getUser(@CurrentUser() user: UserDocument) {
+    return user;
   }
 }
